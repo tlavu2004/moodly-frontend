538a298f8ca783ea21c1ed08729a606f5fb7cd51
Trương Lê Anh Vũ <truongleanhvu20052004@gmail.com>
2026-09-17 10:40:32 +0700

    feat(auth0): implement AuthBootstrap component for handling authentication states

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { Auth0Provider } from '@auth0/auth0-react'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import { AuthBootstrap } from './auth/AuthBootstrap.tsx'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
@@ -14,7 +15,9 @@ createRoot(document.getElementById('root')!).render(
         redirect_uri: window.location.origin,
       }}
     >
-      <App />
+      <AuthBootstrap>
+        <App />
+      </AuthBootstrap>
     </Auth0Provider>
   </StrictMode>,
 )
